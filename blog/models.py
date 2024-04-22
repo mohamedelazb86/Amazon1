9197c9991c2d2ab3e37882fa9243e1a5b6422e73
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-04-22 12:36:45 +0200

    create database for posts and add tags

---
+++ blog/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from django.utils.text import slugify
+from taggit.managers import TaggableManager
+
+
+class Post(models.Model):
+    user=models.ForeignKey(User,related_name='post_user',on_delete=models.SET_NULL,null=True,blank=True)
+    title=models.CharField(max_length=120,verbose_name=_('title'))
+    content=models.TextField(max_length=1000)
+    image=models.ImageField(upload_to='photo_post')
+    draft=models.BooleanField(default=True)
+    tags = TaggableManager()
+    publish_date=models.DateTimeField(default=timezone.now)
+    category=models.ForeignKey('Category',related_name='post_category',on_delete=models.CASCADE)
+    slug=models.SlugField(null=True,blank=True)
+
+
+    def __str__(self):
+        return self.title
+    def save(self,*args,**kwargs):
+        self.slug=slugify(self.title)
+        super(Post,self).save(*args,**kwargs)
+
+class Category(models.Model):
+    name=models.CharField(max_length=120)
+
+    def __str_(self):
+        return self.name
+class Review(models.Model):
+    user=models.CharField(max_length=120)
+    post=models.ForeignKey(Post,related_name='review_post',on_delete=models.CASCADE)
+    content=models.TextField(max_length=500)
+    publish_date=models.DateTimeField(default=timezone.now)
+    rate=models.IntegerField(choices=[(i,i) for i in range(1,6)])
+
+    def __str__(self):
+        return f"{self.user}----{self.post}"
+
+
