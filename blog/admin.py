34aa62a3e898920285a3cfb1dd125dffe5105711
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-04-22 12:43:22 +0200

    add adminpanel and summernote editor

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post,Category,Review
 
-# Register your models here.
+class PostAdmin(SummernoteModelAdmin):
+    list_display=['title','draft','publish_date']
+    list_filter=['draft']
+    search_fields=['title']
+
+    summernote_fields = ('content',)
+
+
+admin.site.register(Post,PostAdmin)
+admin.site.register(Category)
+admin.site.register(Review)
+
