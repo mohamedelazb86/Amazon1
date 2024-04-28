625d983aa54efaefb78d75bd033edc2c40be82a3
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-04-28 14:05:25 +0200

    add paginations

--- products/views.py
+++ products/views.py
@@ -5,9 +5,11 @@ from .models import Product,Brand
 
 class Product_List(ListView):
     model=Product
+    paginate_by=24
 
 class Product_Detail(DetailView):
     model=Product
+    
 
 class Brand_List(ListView):
     model=Brand
