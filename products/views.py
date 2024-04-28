c464eaaad7a2153300cf0176b52e75721f5b47d1
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-04-28 17:01:38 +0200

    add product detail

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
-from .models import Product,Brand
+from .models import Product,Brand,Review,ImagProduct
 
 
 class Product_List(ListView):
@@ -9,7 +9,18 @@ class Product_List(ListView):
 
 class Product_Detail(DetailView):
     model=Product
+
+
+    def get_context_data(self, **kwargs) :
+        context = super().get_context_data(**kwargs)
+        context["reviews"] = Review.objects.filter(product=self.get_object()).order_by('-id')[:3]
+        context["imagess"] = ImagProduct.objects.filter(product=self.get_object())
+        context["products"] =Product.objects.filter(brand=self.get_object().brand)
+
+        
+        return context
     
+
 
 class Brand_List(ListView):
     model=Brand
